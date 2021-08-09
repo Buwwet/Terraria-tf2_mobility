edb6f1e77ea691a9dd068ee7ed960f11fef66bc8
Buwwet <59463043+Buwwet@users.noreply.github.com>
2021-08-09 09:35:49 -0500

    add files

---
+++ Projectiles/Rocket.cs
@@ -0,0 +1,16 @@
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Microsoft.Xna.Framework;
+
+namespace tf2_mobility.Projectiles {
+    public class Rocket : ModProjectile {
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Rocket");
+        }
+
+        
+    }
+}
